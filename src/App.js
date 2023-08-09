f040c5d1a97a9d90bf121f0ec88822bebf39c7d8
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-08-08 19:15:57 -0500

    In this commit, I merged the player component with the playermusic component, creating a unified solution. The next steps involve enhancing image loading and beginning work on the recording feature.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import About from './pages/About/About';
-import MusicPlayer from './pages/MusicPlayer/MusicPlayer';
 import AboutPortfolio from './pages/AboutPortfolio/AboutPortfolio';
 import Search from './pages/Search/Search';
 import { MusicPlayerProvider } from './MusicPlayerContext';
@@ -16,7 +15,6 @@ function App() {
         <Route path='/' exact   element={<Home/>} />
         <Route path='/about/:id' exact   element={<About/>} />
         <Route path='/aboutPortfolio/:id' exact   element={<AboutPortfolio/>} />
-        <Route path='/musicPlayer/:id/*' exact element={<MusicPlayer/>}/>
         <Route path='/search' exact element={<Search/>}/>
       </Routes>
       </MusicPlayerProvider>
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,33 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Player.css';
 import { FaHeart, FaPlay, FaPause } from 'react-icons/fa';
 import { BsPcDisplay } from 'react-icons/bs';
-import { useNavigate } from 'react-router-dom';
+import { AiOutlineClose } from 'react-icons/ai';
+import { BiSkipNext, BiSkipPrevious } from 'react-icons/bi';
 import { useMusicPlayer } from '../../MusicPlayerContext';
 
 const Player = (props) => {
   const { isPlaying, setIsPlaying, audioRef, currentTime, setCurrentTime } = useMusicPlayer();
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/musicPlayer/${props.id}`, {
-      state: {
-        id: props.id,
-        img: props.img,
-        alt: props.alt,
-        song: props.song,
-        artist: props.artist,
-        url: props.url,
-        lyrics: props.lyrics,
-      },
-    });
-  };
-
-  useEffect(() => {
-    if (props.url) {
-      setIsPlaying(true);
-    }
-  }, [props.url]);
+  const [expanded, setExpanded] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [volume, setVolume] = useState(1);
+  const progressBarRef = React.createRef();
 
   const handlePlay = () => {
     setIsPlaying(!isPlaying);
@@ -41,40 +25,121 @@ const Player = (props) => {
     }
   }, [isPlaying]);
 
-  // Actualizar el tiempo de reproducción en tiempo real
+  // Update time in real-time
   useEffect(() => {
     const handleTimeUpdate = () => {
       setCurrentTime(audioRef.current.currentTime);
     };
-
-    audioRef.current.addEventListener('timeupdate', handleTimeUpdate);
-
+  
+    if (audioRef.current) {
+      audioRef.current.addEventListener('timeupdate', handleTimeUpdate);
+    }
+  
     return () => {
-      audioRef.current.removeEventListener('timeupdate', handleTimeUpdate);
+      if (audioRef.current) {
+        audioRef.current.removeEventListener('timeupdate', handleTimeUpdate);
+      }
     };
   }, [audioRef, setCurrentTime]);
+  
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleProgressBarChange = () => {
+    const currentTime = progressBarRef.current.value;
+    audioRef.current.currentTime = currentTime;
+    setCurrentTime(currentTime);
+  };
+
+  const handleVolumeChange = (e) => {
+    const volume = e.target.value;
+    audioRef.current.volume = volume;
+    setVolume(volume);
+  };
+
+  useEffect(() => {
+    setVolume(audioRef.current.volume);
+  }, []);
 
   return (
-    <div className='player-container'>
-      <div className='song-container' onClick={handleClick}>
-        <div className='song-img'>
+    <div className={`player-container ${expanded ? 'expanded' : ''}`}>
+      <div className='song-container' onClick={toggleExpanded}>
+        <div className='song-img' >
           <img src={props.img} alt={props.alt} />
         </div>
         <div className='song-info'>
           <span className='song-name'>{props.song}</span>
-          <span>{props.artist}</span>
         </div>
       </div>
-      <div className='player-controls'>
-        <BsPcDisplay />
-        <FaHeart />
-        <div className='play-pause-btn' onClick={handlePlay}>
-          {isPlaying ? <FaPause /> : <FaPlay />}
+      {!expanded && ( <div className='player-controls'>
+          <BsPcDisplay />
+          <FaHeart />
+          <div className='play-pause-btn' onClick={handlePlay}>
+            {isPlaying ? <FaPause /> : <FaPlay />}
+          </div>
+        </div>)}
+      {expanded && (
+        <div>
+        {expanded && (
+          <div className='return-btn'>
+            <AiOutlineClose onClick={toggleExpanded} />
+          </div>
+        )}
+        <div className='song-container-expanded' >
+        <div className='song-img-expanded' onClick={toggleExpanded}>
+          <img src={props.img} alt={props.alt} />
+        </div>
+        <div className='song-info-expanded'>
+          <span className='song-name-expanded'>{props.song}</span>
         </div>
       </div>
-      <audio ref={audioRef} src={props.url}  onEnded={() => setIsPlaying(false)} />
+          <div className='progress-bar-container'>
+            <span className='time-display'>{formatTime(currentTime)}</span>
+            <input
+              className='progress-bar'
+              ref={progressBarRef}
+              type='range'
+              min='0'
+              max={duration}
+              step='0.01'
+              value={currentTime}
+              onChange={handleProgressBarChange}
+            />
+            <span className='time-display time-duration'>{formatTime(duration)}</span>
+          </div>
+          <div className='audio-player'>
+            <BiSkipPrevious />
+            <div className='play-pause-btn' onClick={handlePlay}>
+              {isPlaying ? <FaPause /> : <FaPlay />}
+            </div>
+            <BiSkipNext />
+          </div>
+          <div className='volume-bar-container'>
+            <input type='range' min='0' max='1' step='0.01' value={volume} onChange={handleVolumeChange} />
+          </div>
+          <div className='lyrics-container'>
+            <p className='lyrics'>{props.lyrics}</p>
+          </div>
+        </div>
+      )}
+      <audio
+        ref={audioRef}
+        src={props.url}
+        onEnded={() => setIsPlaying(false)}
+        onLoadedMetadata={(e) => {
+          setDuration(e.target.duration);
+        }}
+      />
     </div>
   );
+};
+
+// Helper function to format time in mm:ss format
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 };
 
 export default Player;
